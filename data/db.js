cf6b95005a822e903363d3cb8c1f7f92f8ab4202
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-20 17:27:58 +0100

    FIX password

--- data/db.js
+++ data/db.js
@@ -3,11 +3,13 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "blog",
+  password: "andnow71!!",
+  database: "blog_db",
 });
 
 connection.connect((err) => {
   if (err) throw err;
   console.log("Connected!");
 });
+
+module.exports = connection;
